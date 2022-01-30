6d8c72764f5a381d05c9d05a29faf0a08cd5e63e
Kaustubh Kulkarni <kaustubh@pop-os.localdomain>
2022-01-31 04:27:00 +0530

    v emacs: init: elpa packages

---
+++ vanilla_emacs/.emacs.d/elpa/lsp-mode-20220129.1938/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20220129.1938" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "cb7ec94435e6b8cc0b5686a2fdabb1c87260920f" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
