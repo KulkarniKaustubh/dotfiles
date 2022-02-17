bd34d64a5a6f57b3c6d5f1d7b8b715a6603208ea
Kaustubh Kulkarni <kmkapril15@gmail.com>
2022-02-17 15:54:10 +0530

    emacs: init: remove persp mode, add impatient mode and github markdown

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -63,7 +63,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes '(default))
  '(package-selected-packages
-   '(persp-mode iedit pyvenv rainbow-delimiters evil-collection evil company-box company flycheck lsp-ui lsp-mode golden-ratio vterm-toggle vterm general smex helpful undo-tree counsel ivy-rich doom-themes which-key magit doom-modeline ivy use-package)))
+   '(impatient-mode iedit pyvenv rainbow-delimiters evil-collection evil company-box company flycheck lsp-ui lsp-mode golden-ratio vterm-toggle vterm general smex helpful undo-tree counsel ivy-rich doom-themes which-key magit doom-modeline ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -292,9 +292,35 @@
 (use-package iedit)
 
 ;; For workspaces, perspective or persp-mode
-(use-package persp-mode
+;; (use-package persp-mode
+;;   :config
+;;   (persp-mode 1))
+
+(use-package impatient-mode
+  :hook
+  (markdown-mode . impatient-mode)
+  (markdown-mode . httpd-start)
+  :functions markdown-html
   :config
-  (persp-mode 1))
+  (defun impatient-github-markdown-filter (buffer)
+    (princ
+    (with-temp-buffer
+        (let ((tmp (buffer-name)))
+        (set-buffer buffer)
+        (set-buffer (markdown tmp))
+        (format "<!DOCTYPE html><html><title>Markdown preview</title><link rel=\"stylesheet\" href = \"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css\"/>
+    <body><article class=\"markdown-body\" style=\"box-sizing: border-box;min-width: 200px;max-width: 980px;margin: 0 auto;padding: 45px;\">%s</article></body></html>" (buffer-string))))
+    (current-buffer)))
+
+  (defun github-markdown-preview ()
+    "Github markdown preview."
+    (interactive)
+    (unless (process-status "httpd")
+        (httpd-start))
+    (impatient-mode)
+    (imp-set-user-filter 'impatient-github-markdown-filter)
+    (imp-visit-buffer))
+  )
 
 ;; Load my general package key bindings
 (load "~/.emacs.d/general_keys.el")
