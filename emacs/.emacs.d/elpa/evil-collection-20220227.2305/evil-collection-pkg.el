dda12315353a18363e918555f63ee260129d69f3
Kaustubh Kulkarni <kmkapril15@gmail.com>
2022-03-05 17:12:11 +0530

    emacs: elpa stuff

---
+++ emacs/.emacs.d/elpa/evil-collection-20220227.2305/evil-collection-pkg.el
@@ -0,0 +1,14 @@
+(define-package "evil-collection" "20220227.2305" "A set of keybindings for Evil mode"
+  '((emacs "25.1")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "412a908104d1d238e903b96c32deea4954f41876" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
