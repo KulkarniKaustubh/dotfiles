967d15bf7b6a948c68fa5f4ec6fef69413400fbe
Kaustubh Kulkarni <kmkapril15@gmail.com>
2022-03-07 14:30:40 +0530

    emacs: elpa stuff

---
+++ emacs/.emacs.d/elpa/names-20180321.1155/names-pkg.el
@@ -0,0 +1,13 @@
+(define-package "names" "20180321.1155" "Namespaces for emacs-lisp. Avoid name clobbering without hiding symbols."
+  '((emacs "24.1")
+    (cl-lib "0.5"))
+  :commit "d8baba5360e5253938a25d3e005455b6d2d86971" :authors
+  '(("Artur Malabarba" . "emacs@endlessparentheses.com"))
+  :maintainer
+  '("Artur Malabarba" . "emacs@endlessparentheses.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/Malabarba/names")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/.emacs.d/elpa/unicode-escape-20160614.1234/unicode-escape-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/kaustubh/.emacs.d/elpa/unicode-escape-20160614.1234/unicode-escape.el  -*- no-byte-compile: t -*-
+(define-package "unicode-escape" "20160614.1234" "Escape/Unescape unicode notations" '((emacs "24") (names "20151201.0") (dash "2.12.1")) :commit "fc69ec780d9e54c364a9252bd0cf1d2507f3fab7" :authors '(("KOBAYASHI Shigeru (kosh)" . "shigeru.kb@gmail.com")) :maintainer '("KOBAYASHI Shigeru (kosh)" . "shigeru.kb@gmail.com") :keywords '("i18n" "unicode") :url "https://github.com/kosh04/unicode-escape.el")
