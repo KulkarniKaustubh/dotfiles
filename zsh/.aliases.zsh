db28cccdd8a3f876575b3cd79474cb716308bf08
Kaustubh Kulkarni <kmkapril15@gmail.com>
2022-04-03 12:58:02 +0530

    zsh: aliases: safer rm command

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -18,13 +18,12 @@ alias p="python"
 alias p3="python3"
 alias cls="clear"
 
-alias srm="gio trash" # safe rm command
+alias rm="gio trash" # safe rm command
 
 alias ec="emacsclient -c"
 alias ecnw="TERM=xterm-direct emacsclient -c -nw"
 alias e="emacs"
 alias enw="TERM=xterm-direct emacs -nw"
-
 alias erestart="emacsclient -e '(save-buffers-kill-emacs)'; emacs --daemon"
 
 alias keys="xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf \"%-3s %s\n\", $5, $8 }'"
