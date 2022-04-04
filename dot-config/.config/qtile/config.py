ad99baca4ba63ea12351b849b58eaae19c295618
Kaustubh Kulkarni <kmkapril15@gmail.com>
2022-04-04 10:47:46 +0530

    dot-config/.config/qtile/: add calendar

--- dot-config/.config/qtile/config.py
+++ dot-config/.config/qtile/config.py
@@ -542,7 +542,12 @@ screens = [
                     foreground="#152238", background="#ffffff", fontsize=10
                 ),
                 widget.Spacer(length=bar.STRETCH),
-                widget.Clock(format="%A | %I:%M | %B %d"),
+                widget.Clock(
+                    format="%A | %I:%M | %B %d",
+                    mouse_callbacks={
+                        "Button1": lambda: qtile.cmd_spawn("gnome-calendar")
+                    },
+                ),
                 widget.Spacer(length=bar.STRETCH),
                 widget.TextBox(
                     text="Apps",
@@ -743,7 +748,12 @@ screens = [
                     foreground="#152238", background="#ffffff", fontsize=10
                 ),
                 widget.Spacer(length=bar.STRETCH),
-                widget.Clock(format="%A | %I:%M | %B %d"),
+                widget.Clock(
+                    format="%A | %I:%M | %B %d",
+                    mouse_callbacks={
+                        "Button1": lambda: qtile.cmd_spawn("gnome-calendar")
+                    },
+                ),
                 widget.Spacer(length=bar.STRETCH),
                 widget.TextBox(
                     text="GPU Temp",
@@ -924,6 +934,7 @@ floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
+        Match(wm_class="gnome-calendar"),  # Calendar app
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
