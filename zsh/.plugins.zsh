d443272072b870ab90c76b77b3de3d4fc35d0357
Kaustubh Kulkarni <kmkapril15@gmail.com>
2024-01-22 19:10:57 -0700

    .zshrc: separate plugins, add kube alias

---
+++ zsh/.plugins.zsh
@@ -0,0 +1,47 @@
+# Downloading and sourcing themes, plugins, etc.
+# check if ~/.zsh exists
+if [ ! -d "$HOME/.zsh" ]; then
+    echo "Creating a .zsh folder in $HOME"
+    echo "This can be copied elsewhere and then linked, preferrably using GNU stow"
+    mkdir $HOME/.zsh
+fi
+
+# check if p10k exists
+if [ ! -d "$HOME/.zsh/powerlevel10k" ]; then
+    echo "Installing powerelevel10k theme."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.zsh/powerlevel10k
+fi
+
+source $HOME/.zsh/powerlevel10k/powerlevel10k.zsh-theme
+
+# check if zsh autosuggest exists
+if [ ! -d "$HOME/.zsh/zsh-autosuggestions" ]; then
+    echo "Installing zsh autosuggestions."
+    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+fi
+
+source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# check if zsh syntax highlighting exists
+if [ ! -d "$HOME/.zsh/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh syntax highlighting."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
+fi
+
+source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# check if zsh history susbtring search
+if [ ! -d "$HOME/.zsh/zsh-history-substring-search" ]; then
+    echo "Installing zsh history substring search."
+    git clone https://github.com/zsh-users/zsh-history-substring-search.git $HOME/.zsh/zsh-history-substring-search
+fi
+
+source $HOME/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+# check if fzf dir navigator exists exists
+if [ ! -d "$HOME/.zsh/fzf-dir-navigator" ]; then
+    echo "Installing fzf dir navigator."
+    git clone https://github.com/KulkarniKaustubh/fzf-dir-navigator.git $HOME/.zsh/fzf-dir-navigator
+fi
+
+source $HOME/.zsh/fzf-dir-navigator/fzf-dir-navigator.zsh
